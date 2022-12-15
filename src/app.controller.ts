a5eb7ffb01cff3a5cb0bf94a1a0b1e153cf15302
Marcela-Rodrigues <andradem45678@gmail.com>
2022-12-15 08:20:14 -0300

    new: prisma, routes and validations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get , Post} from '@nestjs/common';
+import { Body } from '@nestjs/common/decorators';
+import { randomUUID } from 'node:crypto';
 import { AppService } from './app.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list(){
+    return this.prismaService.notification.findMany()
+  }
+
+  @Post()
+  async create(@Body() body:CreateNotificationBody){
+    console.log(body)
+    const {content, category,recipientId  } = body
+    await this.prismaService.notification.create({
+      data:{
+        id: randomUUID(),
+        content,
+        category,
+        recipientId
+      }
+    })
+    return this.prismaService.notification.findMany()
   }
 }
